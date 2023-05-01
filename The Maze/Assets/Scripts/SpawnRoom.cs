23deacdf7bddb8c786f83bc6be47d515bddf77ad
suchencjusz <sucheta348@gmail.com>
2023-05-01 21:08:15 +0200

    Generowanie mapy prawie dziala, nie skonczone

--- The Maze/Assets/Scripts/SpawnRoom.cs
+++ The Maze/Assets/Scripts/SpawnRoom.cs
@@ -6,11 +6,14 @@ public class SpawnRoom : MonoBehaviour
 {
     public LayerMask room;
     public LevelGeneration levelGen;
-    private void Update() {
+
+    private void Update()
+    {
         Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
-        if(roomDetection==null && levelGen.stopGeneration==true) {
+        if (roomDetection == null && levelGen.stopGeneration == true)
+        {
             int rand = Random.Range(0, levelGen.rooms.Length);
-           Instantiate(levelGen.rooms[rand], transform.position, Quaternion.identity);
+            Instantiate(levelGen.rooms[rand], transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
